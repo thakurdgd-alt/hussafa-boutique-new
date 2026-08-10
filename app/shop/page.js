23de941a922c59e23d1ebdf6139b85da6b103837
thakurdgd-alt <thakurdgd@gmail.com>
2026-08-10 13:10:44 +0500

    Update page.js

--- app/shop/page.js
+++ app/shop/page.js
@@ -19,7 +19,7 @@ const products = [
     price: "Coming Soon",
     description:
       "Stylish handbags and colorful shoulder purses designed for everyday elegance.",
-    image: "/bags-collection.jpg?v=1",
+    image: "/images24.jpg?v=1",
   },
 ];
 
